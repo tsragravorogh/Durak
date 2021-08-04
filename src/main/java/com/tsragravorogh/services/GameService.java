c6b2c15ce9cd2779beef57dc4d6c4a3540105784
tsragravorogh <71145107+tsragravorogh@users.noreply.github.com>
2021-08-04 18:52:10 +0300

    GameService was created

---
+++ src/main/java/com/tsragravorogh/services/GameService.java
@@ -0,0 +1,11 @@
+package com.tsragravorogh.services;
+
+import com.tsragravorogh.elements.Game;
+
+public class GameService {
+    public void play(Game g, int playersCount) {
+        //TODO 'initPlayers'
+        //TODO 'initCards'
+        //TODO 'dealCardsToPlayers'
+    }
+}
