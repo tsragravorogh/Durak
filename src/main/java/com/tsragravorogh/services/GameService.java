5f544f10b931ebd127a22872f9bdb646a4091223
tsragravorogh <71145107+tsragravorogh@users.noreply.github.com>
2021-08-05 18:44:39 +0300

    The choice of the user who plays has created.

--- src/main/java/com/tsragravorogh/services/GameService.java
+++ src/main/java/com/tsragravorogh/services/GameService.java
@@ -4,14 +4,15 @@ import com.tsragravorogh.elements.*;
 import com.tsragravorogh.utils.CyclicLinkedList;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class GameService {
     public void play(Game g, int playersCount) {
         initPlayers(g, playersCount);
         initCards(g);
         dealCardsToPlayers(g);
+        //initGame(g);
         // TODO init the game
-        System.out.println(g.getPlayers().toString());
     }
 
     private void initPlayers(Game g, int count) {
@@ -47,10 +48,8 @@ public class GameService {
     }
 
     private void dealCardsToPlayers(Game g){
-        Map<Player, ArrayList<Card>> cardsPlayer = new HashMap<>();
         CyclicLinkedList<Player> players = g.getPlayers();
         LinkedList<Card> deck = g.getDeck();
-
         for(int i = 0; i < players.size(); i++) {
             ArrayList<Card> cardsToPlayer = new ArrayList<>(6);
             for(int j = 0; j < 6; j++) {
@@ -63,25 +62,76 @@ public class GameService {
         g.setDeck(deck);
     }
 
-    //init the game
+    // TODO init the game
 
     private void initGame(Game g) {
         while (isGameAlive(g)) {
-            
+            Player source = null;
+            Player target;
+
+            boolean isPickedUp = false;
+            //TODO choice of the user who plays first
+            if(isFirstRound(g)){
+                source = getFirstSourcePlayer(g);
+                target = g.getPlayers().getNext(source);
+            }
+            else{
+                source = getSourcePlayer(g, isPickedUp, source);
+                target = getTargetPlayer(g, source);
+            }
         }
+    }
+
+    private Player getFirstSourcePlayer(Game g) {
+        CyclicLinkedList<Player> players = g.getPlayers();
+        Map<Player, Card> highCard = new HashMap<>();
+        Optional<Map.Entry<Player, Card>> high;
+        if(g.getTrump().getFace().getRank() < 11) {
+            for (int i = 0; i < players.size(); i++) {
+                ArrayList<Card> cards = players.get(i).getPlayerCards();
+
+                cards.stream().filter(c -> c.getSuit() == g.getTrump().getSuit()).filter(c -> c.getFace().getRank() > g.getTrump().getFace().getRank()).collect(Collectors.toCollection(ArrayList::new));
+                Optional<Card> c = cards.stream().max(Comparator.comparingInt(e -> e.getFace().getRank()));
+                if(c.isPresent()) {
+                    highCard.put(players.get(i), c.get());
+                }
+            }
+            high = highCard.entrySet().stream().max(Comparator.comparingInt(e -> e.getValue().getFace().getRank()));
+        }else{
+            for (int i = 0; i < players.size(); i++) {
+                ArrayList<Card> cards = players.get(i).getPlayerCards();
+
+                cards.stream().filter(c -> c.getSuit() == g.getTrump().getSuit()).filter(c -> c.getFace().getRank() < g.getTrump().getFace().getRank()).collect(Collectors.toCollection(ArrayList::new));
+                Optional<Card> c = cards.stream().max(Comparator.comparingInt(e -> e.getFace().getRank()));
+
+                if(c.isPresent()){
+                    highCard.put(players.get(i), c.get());
+                }
+            }
+            high = highCard.entrySet().stream().max(Comparator.comparingInt(e -> e.getValue().getFace().getRank()));
+        }
+        return high.get().getKey();
+    }
+
+    private boolean isFirstRound(Game g) {
+        return g.getFightHistory() == null;
     }
 
     private boolean isGameAlive(Game g){
         return g.getPlayers().size() > 1;
     }
 
-    private Player getSourcePlayer(Game g, boolean isPickedUp, Player playerBefore) {
+    private Player getSourcePlayer(Game g, boolean isPickedUp, Player previousSource) {
         Player player;
         if (!isPickedUp) {
-            player = g.getNext(playerBefore);
+            player = g.getNext(previousSource);
         } else {
-            player = g.getNext(g.getNext(playerBefore));
+            player = g.getNext(g.getNext(previousSource));
         }
         return player;
+    }
+
+    private Player getTargetPlayer(Game g, Player sourcePlayer) {
+        return g.getNext(sourcePlayer);
     }
 }
